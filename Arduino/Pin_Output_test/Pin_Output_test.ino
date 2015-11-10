9e922735849edca71d52ede86fc04f792f3d369f
cfisher <cfisher@iastate.edu>
2015-11-10 15:09:33 -0600

    Adding initial files

---
+++ Arduino/Pin_Output_test/Pin_Output_test.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //5 Volts
+  //digitalWrite(8, HIGH);
+  //digitalWrite(8, LOW);
+  //2.5 V ish
+  //analogWrite(10, 128);
+  analogWrite(10, 168);
+  
+}
